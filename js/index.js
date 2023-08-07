2c83f49c548439436706173a7608b764285b46df
Jacy Neto <jacy.neeto@gmail.com>
2023-08-07 10:11:09 -0300

    Atualizando versão

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import calculate from "./calculate.js"
+import copyToClipboard from "./copyToClipboard.js"
+import { handleButtonPress, handleClearButton, handleTyping } from "./keyHandler.js"
+import switchTheme from "./switchTheme.js"
+
+document.querySelectorAll('.charKey').forEach(function(charKeyBtn) {
+    charKeyBtn.addEventListener('click', handleButtonPress)
+})
+document.getElementById('clear').addEventListener('click', handleClearButton)
+document.getElementById('input').addEventListener('keydown' , handleTyping)
+document.getElementById('equal').addEventListener('click', calculate)
+document.getElementById('copyToClipboard').addEventListener('click', copyToClipboard)
+document.getElementById('switchTheme').addEventListener('click', switchTheme)
